298f2a3ab20da7dc944dc486ee55f3f08ce8e549
Mark Miles <mark.miles@students.fhu.edu>
2024-02-01 12:35:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 #Remove pass and complete the code
 def check_character(word, index):
-   pass
+   if word[index].isalpha() == True:
+       statement = 'letter'
+       return statement
+   elif word[index].isspace() == True:
+       statement = 'white space'
+       return statement
+   elif word[index].isdigit() == True:
+       statement = 'digit'
+       return statement
+   else:
+       statement = 'unknown'
+       return statement
 
 if __name__ == '__main__': 
     print(check_character('happy birthday', 2))
