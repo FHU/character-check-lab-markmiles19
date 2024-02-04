c399b32a9a6acd1926ad5572e01fd9d5e3a57f92
Mark Miles <mark.miles@students.fhu.edu>
2024-02-04 17:59:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def check_character(word, index):
        return statement
 
 if __name__ == '__main__': 
-    print(check_character('happy birthday', 1))
+    print(check_character('happy birthday', 2))
     print(check_character('happy birthday', 5))
     print(check_character('happy birthday 2 you', 15))
     print(check_character('happy birthday!', 14))
